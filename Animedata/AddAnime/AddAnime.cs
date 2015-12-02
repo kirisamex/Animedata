0b9800339f8a77f35c115f5ebfd1825c359ea980
kirisamex <hyh5678901@gmail.com>
2015-12-03 00:37:28 +0800

    重置刷新

--- Animedata/AddAnime/AddAnime.cs
+++ Animedata/AddAnime/AddAnime.cs
@@ -110,7 +110,7 @@ namespace Main
             if (CommandAnimeInfo(ctr) == true)
             {
                 this.Close();
-                mainform.DataGridViewReload();
+                //mainform.DataGridViewReload();
             }
 
         }
