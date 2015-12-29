bf0c2b73df682652e060ccb91d5588c82b5aa217
kirisamex <hyh5678901@gmail.com>
2015-12-29 15:45:05 +0800

    #3_搜索模块建立

--- Animedata/Main/Maindao.cs
+++ Animedata/Main/Maindao.cs
@@ -3,6 +3,7 @@ using System.Data;
 using System.Text;
 using System.Data.SqlClient;
 using System.Collections.Generic;
+using Main.Lib.Model;
 
 namespace Main
 {
@@ -587,6 +588,238 @@ namespace Main
         }
 
         /// <summary>
+        /// 动画界面加载
+        /// 搜索
+        /// </summary>
+        /// <param name="search"></param>
+        /// <returns></returns>
+        public DataSet Getanime(SearchModule search)
+        {
+            SqlDataAdapter adp = new SqlDataAdapter();
+            SqlCommand cmd = new SqlCommand();
+
+            StringBuilder sqlmaincmd = new StringBuilder();
+            StringBuilder joincmd = new StringBuilder();
+
+            sqlmaincmd.Append( @"SELECT DISTINCT
+                                    AT.ANIME_NO,
+                                    AT.ANIME_CHN_NAME, 
+                                    AT.ANIME_JPN_NAME,
+                                    AT.ANIME_NN,
+                                    AT.STATUS,
+									AT.ORIGINAL
+                                    FROM ANIMEDATA.dbo.T_ANIME_TBL AT
+                                    INNER JOIN ANIMEDATA.dbo.T_PLAYINFO_TBL PT ON AT.ANIME_NO = PT.ANIME_NO 
+									");
+
+            
+
+            #region 动画编号
+            //动画编号
+            if (!string.IsNullOrEmpty(search.animeNo))
+            {
+                AddWhereAnd(joincmd);
+                joincmd.Append(@" AT.ANIME_NO = @anime_no ");
+
+                cmd.Parameters.Add("@anime_no", search.animeNo);
+            }
+            #endregion
+
+            #region 中文名
+            //中文名
+            if (!string.IsNullOrEmpty(search.animeCNName))
+            {
+                AddWhereAnd(joincmd);
+                joincmd.Append(SQLAndBuilder(search.animeCNNameSearchWay, "AT.ANIME_CHN_NAME", "anime_cn_name"));
+
+                cmd.Parameters.Add(AddParam(search.animeCNNameSearchWay, "anime_cn_name", search.animeCNName));
+            }
+            #endregion
+
+            #region 日文名
+            //日文名
+            if (!string.IsNullOrEmpty(search.animeJPName))
+            {
+                AddWhereAnd(joincmd);
+                joincmd.Append(SQLAndBuilder(search.animeCNNameSearchWay, "AT.ANIME_JPN_NAME", "anime_jp_name"));
+
+                cmd.Parameters.Add(AddParam(search.animeJPNameSearchWay, "anime_jp_name", search.animeJPName));
+            }
+            #endregion
+
+            #region 动画简写
+            //动画简写
+            if (!string.IsNullOrEmpty(search.animeNN))
+            {
+                AddWhereAnd(joincmd);
+                joincmd.Append(@" AT.ANIME_NN = @anime_nn ");
+
+                cmd.Parameters.Add("@anime_no", search.animeNN);
+            }
+            #endregion
+
+            #region 播放时间
+            //播放时间
+            if (!(search.animePlaytimeFrom == DateTime.MinValue || search.animePlaytimeFrom == DateTime.MaxValue
+                    || search.animePlaytimeTo == DateTime.MinValue || search.animePlaytimeTo == DateTime.MaxValue))
+            {
+                AddWhereAnd(joincmd);
+
+                //确定搜索规则
+                //FromTo
+                if ((search.animePlaytimeFrom != DateTime.MaxValue && search.animePlaytimeFrom != DateTime.MinValue) &&
+                        (search.animePlaytimeTo != DateTime.MinValue && search.animePlaytimeTo != DateTime.MaxValue))
+                {
+                    search.animePlaytimeSearchRule = SearchModule.DateTimeSearchRule.FromTo;
+                    cmd.Parameters.Add(AddParam("anime_playtimefrom", search.animePlaytimeFrom));
+                    cmd.Parameters.Add(AddParam("anime_playtimeto", search.animePlaytimeTo));
+                }
+
+                //From
+                if (search.animePlaytimeFrom != DateTime.MaxValue && search.animePlaytimeFrom != DateTime.MinValue)
+                {
+                    search.animePlaytimeSearchRule = SearchModule.DateTimeSearchRule.From;
+                    cmd.Parameters.Add(AddParam("anime_playtimefrom", search.animePlaytimeFrom));
+                }
+                //To
+                else
+                {
+                    search.animePlaytimeSearchRule = SearchModule.DateTimeSearchRule.To;
+                    cmd.Parameters.Add(AddParam("anime_playtimeto", search.animePlaytimeTo));
+                }
+
+                joincmd.Append(SQLAndBuilder(search.animePlaytimeSearchWay, search.animePlaytimeSearchRule,
+                        "anime_playtimefrom", "anime_playtimeto", "PT.START_TIME"));
+
+
+            }
+            #endregion
+
+            #region 收看时间
+            //收看时间
+            if (!(search.animeWatchtimeFrom == DateTime.MinValue || search.animeWatchtimeFrom == DateTime.MaxValue
+                    || search.animeWatchtimeTo == DateTime.MinValue || search.animeWatchtimeTo == DateTime.MaxValue))
+            {
+                AddWhereAnd(joincmd);
+
+                //确定搜索规则
+                //FromTo
+                if ((search.animeWatchtimeFrom != DateTime.MaxValue && search.animeWatchtimeFrom != DateTime.MinValue) &&
+                        (search.animeWatchtimeTo != DateTime.MinValue && search.animeWatchtimeTo != DateTime.MaxValue))
+                {
+                    search.animeWatchtimeSearchRule = SearchModule.DateTimeSearchRule.FromTo;
+                    cmd.Parameters.Add(AddParam("anime_watchtimefrom", search.animeWatchtimeFrom));
+                    cmd.Parameters.Add(AddParam("anime_watchtimeto", search.animeWatchtimeTo));
+                }
+
+                //From
+                if (search.animeWatchtimeFrom != DateTime.MaxValue && search.animeWatchtimeFrom != DateTime.MinValue)
+                {
+                    search.animeWatchtimeSearchRule = SearchModule.DateTimeSearchRule.From;
+                    cmd.Parameters.Add(AddParam("anime_watchtimefrom", search.animeWatchtimeFrom));
+                }
+                //To
+                else
+                {
+                    search.animeWatchtimeSearchRule = SearchModule.DateTimeSearchRule.To;
+                    cmd.Parameters.Add(AddParam("anime_watchtimeto", search.animeWatchtimeTo));
+                }
+
+                joincmd.Append(SQLAndBuilder(search.animeWatchtimeSearchWay, search.animeWatchtimeSearchRule,
+                        "anime_watchtimefrom", "anime_watchtimeto", "PT.WATCH_TIME"));
+
+
+            }
+            #endregion
+
+            #region 播放状态
+            //播放状态
+            AddWhereAnd(joincmd);
+            joincmd.Append(" AT.STATUS IN ( ");
+            
+            StringBuilder statusincmd =new StringBuilder();
+
+            if (search.animeStatue.playing)
+            {
+                AddComma(statusincmd);
+                statusincmd.Append(AnimeStatusModule.PLAYING.ToString());
+            }
+            if (search.animeStatue.finished)
+            {
+                AddComma(statusincmd);
+                statusincmd.Append(AnimeStatusModule.FINISHED.ToString());
+            }
+            if (search.animeStatue.newproject)
+            {
+                AddComma(statusincmd);
+                statusincmd.Append(AnimeStatusModule.NEW_PROJECT.ToString());
+            }
+            if (search.animeStatue.discare)
+            {
+                AddComma(statusincmd);
+                statusincmd.Append(AnimeStatusModule.DISCARE.ToString());
+            }
+
+            joincmd.Append(statusincmd);
+            joincmd.Append(" )");
+            #endregion
+
+            #region 原作
+            //播放状态
+            AddWhereAnd(joincmd);
+            joincmd.Append(" AT.ORIGINAL IN ( ");
+
+            StringBuilder originalincmd = new StringBuilder();
+
+            if (search.animeOriginal.fromComic)
+            {
+                AddComma(originalincmd);
+                statusincmd.Append(AnimeOriginalModule.COMIC.ToString());
+            }
+            if (search.animeOriginal.fromNovel)
+            {
+                AddComma(originalincmd);
+                statusincmd.Append(AnimeOriginalModule.NOVEL.ToString());
+            }
+            if (search.animeOriginal.isoriginal)
+            {
+                AddComma(originalincmd);
+                statusincmd.Append(AnimeOriginalModule.ORIGINAL.ToString());
+            }
+            if (search.animeOriginal.fromvideo)
+            {
+                AddComma(originalincmd);
+                statusincmd.Append(AnimeOriginalModule.VIDEO.ToString());
+            }
+            if (search.animeOriginal.fromgame)
+            {
+                AddComma(originalincmd);
+                statusincmd.Append(AnimeOriginalModule.GAME.ToString());
+            }
+            if (search.animeOriginal.fromothers)
+            {
+                AddComma(originalincmd);
+                statusincmd.Append(AnimeOriginalModule.OTHERS.ToString());
+            }
+
+            joincmd.Append(originalincmd);
+            joincmd.Append(" )");
+            #endregion
+
+
+            cmd.CommandText = sqlmaincmd.Append(joincmd.ToString()).ToString();
+            cmd.Connection = Getconnection();
+            adp.SelectCommand = cmd;
+
+            cmd.Connection.Open();
+            DataSet ds = new DataSet();
+            adp.Fill(ds);
+            cmd.Connection.Close();
+
+            return ds;
+        }
+
+        /// <summary>
         /// 播放信息界面DS加载
         /// </summary>
         /// <param name="animeNo"></param>
@@ -655,6 +888,262 @@ namespace Main
             return ds;
         }
 
+        #endregion
+
+        #region MainService
+        /// <summary>
+        /// 字符型检索用SQL文
+        /// </summary>
+        /// <param name="SearchWay">检索方式</param>
+        /// <param name="fieldName">目标字符</param>
+        /// <param name="parameterName">变量名</param>
+        /// <returns></returns>
+        public static string SQLAndBuilder(object SearchWay, string fieldName, string parameterName)
+        {
+            return WhereSentenceBuilder((SearchModule.StringSearchWay)SearchWay, fieldName, parameterName);
+        }
+
+        /// <summary>
+        /// 日期型检索SQL文
+        /// </summary>
+        /// <param name="SearchWay">检索方式</param>
+        /// <param name="SearchRule">检索规则</param>
+        /// <param name="fieldTimeFrom">检索开始时间</param>
+        /// <param name="fieldTimeTo">检索结束时间</param>
+        /// <param name="parameterName">变量名称</param>
+        /// <returns></returns>
+        public static string SQLAndBuilder(object SearchWay, object SearchRule, string fieldTimeFrom, string fieldTimeTo, string parameterName)
+        {
+            return WhereSentenceBuilder((SearchModule.DateTimeSearchWay)SearchWay, (SearchModule.DateTimeSearchRule)SearchRule,
+                    fieldTimeFrom, fieldTimeTo, parameterName);
+        }
+
+        /// <summary>
+        /// 字符型搜索用SQL文:确定检索方式
+        /// </summary>
+        /// <param name="way">检索方式</param>
+        /// <param name="fieldName">目标字符</param>
+        /// <param name="parameterName">变量名</param>
+        /// <returns></returns>
+        public static string WhereSentenceBuilder(SearchModule.StringSearchWay way, string fieldName, string parameterName)
+        {
+            if (string.IsNullOrEmpty(fieldName))
+            {
+                throw new ArgumentException("字段名未设定", "fieldName");
+            }
+
+            switch (way)
+            {
+                case SearchModule.StringSearchWay.Exact:
+                    return CaseOfExactMatch(fieldName, parameterName);
+                case SearchModule.StringSearchWay.Forward:
+                case SearchModule.StringSearchWay.Broad:
+                    return CaseOfAmbiguous(fieldName, parameterName);
+                default:
+                    return string.Empty;
+            }
+        }
+
+        /// <summary>
+        /// 日期型搜索用SQL文
+        /// </summary>
+        /// <param name="way">检索方式</param>
+        /// <param name="rule">检索规则</param>
+        /// <param name="fieldTimeFrom">检索开始时间</param>
+        /// <param name="fieldTimeTo">检索结束时间</param>
+        /// <param name="parameterName">变量名</param>
+        /// <returns></returns>
+        public static string WhereSentenceBuilder(SearchModule.DateTimeSearchWay way, SearchModule.DateTimeSearchRule rule,
+                string fieldTimeFrom, string fieldTimeTo, string parameterName)
+        {
+            return CaseOfRange(way, rule, fieldTimeFrom, fieldTimeTo, parameterName);
+        }
+
+        /// <summary>
+        /// 完全一致检索
+        /// </summary>
+        /// <param name="fieldName">字段名</param>
+        /// <param name="parameterName">变量名</param>
+        /// <returns></returns>
+        private static string CaseOfExactMatch(string fieldName, string parameterName)
+        {
+            return string.Format(@" {0} = @{1} ", fieldName, parameterName);
+        }
+
+        /// <summary>
+        /// 前方、部分检索
+        /// </summary>
+        /// <param name="fieldName">字段名</param>
+        /// <param name="parameterName">变量名</param>
+        /// <returns></returns>
+        private static string CaseOfAmbiguous(string fieldName, string parameterName)
+        {
+            return string.Format(@" {0} LIKE @{1} ", fieldName, parameterName);
+        }
+
+        /// <summary>
+        /// 时间检索
+        /// </summary>
+        /// <param name="way">检索方式</param>
+        /// <param name="rule">检索规则</param>
+        /// <param name="fieldTimeFrom">检索开始时间</param>
+        /// <param name="fieldTimeTo">检索结束时间</param>
+        /// <param name="parameterName">变量名</param>
+        /// <returns></returns>
+        public static string CaseOfRange(SearchModule.DateTimeSearchWay way,SearchModule.DateTimeSearchRule rule,
+                string fieldTimeFrom, string fieldTimeTo, string parameterName)
+        {
+            switch (way)
+            {
+                //▼说明：在此时间内（InThisTime）-目标时间在搜索的时间内
+
+                //||SearchTimeFrom ------------------------------------->SearchTimeTo||
+                //||・・・・・・・・・・・・・・ TargetTime・・・・・・・・・・・・・||
+                //--ToDo：范围时间式样。目前固定为Inthistime
+                //||・・・・・・TargetTimeFrom-------------->TargetTimeTo・・・・・・||
+
+                //▲说明：在此时间内（InThisTime）-目标时间在搜索的时间内
+
+                case SearchModule.DateTimeSearchWay.InThisTime:
+                    {
+                        //From
+                        if (rule == SearchModule.DateTimeSearchRule.From)
+                        {
+                            return string.Format(@" {0} >= @{1} ", parameterName, fieldTimeFrom);
+                        }
+                        //To
+                        if (rule == SearchModule.DateTimeSearchRule.To)
+                        {
+                            return string.Format(@" {0} <= @{1} ", parameterName, fieldTimeTo);
+                        }
+                        //FromTo
+                        if (rule == SearchModule.DateTimeSearchRule.FromTo)
+                        {
+                            return string.Format(
+                                @" {0} >= @{1} AND {0} <= @{2} ", parameterName, fieldTimeFrom, fieldTimeTo);
+                        }
+                        break;
+                    }
+
+                //目前该式样不使用
+                //▼说明：包含此时间（InCludeThisTime）-目标时间包含搜索的时间--ToDo
+
+                //||TargetTimeFrom ------------------------------------->TargetTimeTo||
+                //||・・・・・・SearchTimeFrom-------------->SearchTimeTo・・・・・・||
+
+                //▲说明：包含此时间（InCludeThisTime）-目标时间包含搜索的时间
+
+                //case SearchModule.DateTimeSearchWay.InCludeThisTime:
+                //    {
+                //        break;
+                //    }
+            }
+
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// 设置搜索规则：均有值Fromto，To空From，From空To
+        /// </summary>
+        /// <param name="TimeFrom">搜索开始时间</param>
+        /// <param name="TimeTo">搜索结束时间</param>
+        /// <returns>搜索规则</returns>
+        private static SearchModule.DateTimeSearchRule SetDatetimeSearchRule(DateTime TimeFrom, DateTime TimeTo)
+        {
+            //FromTo
+            if ((TimeFrom != DateTime.MaxValue && TimeFrom != DateTime.MinValue) && (TimeTo != DateTime.MinValue && TimeTo != DateTime.MaxValue))
+            {
+                return SearchModule.DateTimeSearchRule.FromTo;
+            }
+
+            //From
+            if (TimeFrom != DateTime.MaxValue && TimeFrom != DateTime.MinValue)
+            {
+                return SearchModule.DateTimeSearchRule.From;
+            }
+            //To
+            else
+            {
+                return SearchModule.DateTimeSearchRule.To;
+            }
+        }
+
+        /// <summary>
+        /// 模糊检索变量配置
+        /// </summary>
+        /// <param name="way">检索方式</param>
+        /// <param name="targetStr">目标值</param>
+        /// <returns></returns>
+        public static string ReplaceParamValueAmbiguous(SearchModule.StringSearchWay way, string targetStr)
+        {
+            if (way == SearchModule.StringSearchWay.Forward)
+            {
+                return targetStr + "%";
+            }
+            if (way == SearchModule.StringSearchWay.Broad)
+            {
+                return "%" + targetStr + "%";
+            }
+
+            return targetStr;
+        }
+
+        /// <summary>
+        /// 字符型变量赋值
+        /// </summary>
+        /// <param name="way">检索方式</param>
+        /// <param name="pname">变量名</param>
+        /// <param name="pvalue">变量值</param>
+        /// <returns></returns>
+        public static SqlParameter AddParam(SearchModule.StringSearchWay way, string pname, string pvalue)
+        {
+            SqlParameter param = new SqlParameter(pname, SqlDbType.NChar);
+            param.Value = ReplaceParamValueAmbiguous((SearchModule.StringSearchWay)way, pvalue.ToString());
+
+            return param;
+        }
+
+        /// <summary>
+        /// 时间型变量赋值
+        /// </summary>
+        /// <param name="pname">变量名</param>
+        /// <param name="pvalue">变量值</param>
+        /// <returns></returns>
+        public static SqlParameter AddParam(string pname, DateTime pvalue)
+        {
+            SqlParameter param = new SqlParameter(pname, SqlDbType.DateTime);
+            param.Value = new DateTime(pvalue.Year, pvalue.Month, pvalue.Day);
+
+            return param;
+        }
+
+        /// <summary>
+        /// 为WHERE部增加连接字符
+        /// </summary>
+        /// <param name="joincmd"></param>
+        public static void AddWhereAnd(StringBuilder joincmd)
+        {
+            if (joincmd.Length != 0)
+            {
+                joincmd.Append(" AND ");
+            }
+            else
+            {
+                joincmd.Append(" WHERE ");
+            }
+        }
+
+        /// <summary>
+        /// 为IN部增加连接字符
+        /// </summary>
+        /// <param name="whereincmd"></param>
+        public static void AddComma(StringBuilder whereincmd)
+        {
+            if (whereincmd.Length != 0)
+            {
+                whereincmd.Append(@" , ");
+            }
+        }
         #endregion
     }
 }
